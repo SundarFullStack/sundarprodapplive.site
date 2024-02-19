145dabf925153ee3ced42e24e8f03facb6d590ec
SundarFullStack <meenakshisunder183017@gmail.com>
2024-02-19 14:09:17 -0800

    Initialized Commit

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,8 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  base:"/ProductionApp_Frondend_Repository/"
+  base:"/sundarprodapplive.site/",
+  plugins: [react()]
+  
 
 })
